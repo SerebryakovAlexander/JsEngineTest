72f8f8da00dccf354d6c454d9e02a737b032d99c
Alexander Serebryakov <aserebryakov53@gmail.com>
2018-11-23 21:57:20 +0300

    улучшил тесты

--- target/classes/script.js
+++ target/classes/script.js
@@ -1,3 +1,30 @@
-var a = 1;
-var b = 2;
-var c = a + b;
+function ruleMain(inputObject)
+{
+    var rurPayment = JSON.parse(inputObject);
+
+    var result = {};
+
+    result.status = 'OK';
+    result.code = 'VKNOK';
+    result.message = null;
+
+    var a1 = rurPayment.accountBen.substr(0,5);
+    var a2 = rurPayment.accountCli.substr(0,5);
+
+    if (['40702','12345','54321','44444','32145','09876'].indexOf(a1) > -1)
+    {
+        result.code = 'VKOK';
+    }
+
+    if (['40702','12345','54321'].indexOf(a2) > -1)
+    {
+        result.code = 'VKOK';
+    }
+
+    if (rurPayment.narrative.search(/^{VO\d\d\d\d\d}/i) > -1)
+    {
+        result.code = "VKOK";
+    }
+
+    return result;
+}
